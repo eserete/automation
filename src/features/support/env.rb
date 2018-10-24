7f29a5584ae0479f25d15c9d56b2bf7584adaaaf
Eduardo Serete <edu.serete@gmail.com>
2018-10-24 20:54:08 -0300

    Test4

--- src/features/support/env.rb
+++ src/features/support/env.rb
@@ -10,7 +10,7 @@ Capybara.register_driver :selenium_proxy do |app|
   options = Selenium::WebDriver::Chrome::Options.new(args: ['localhost:4444'])
   options.add_argument('--disable-geolocation')
   options.add_argument('--disable-gpu')
-  options.add_argument('port=4444')
+  options.add_argument('--port=4444')
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   Capybara::Selenium::Driver.new(app, :browser => :chrome, options: options)
